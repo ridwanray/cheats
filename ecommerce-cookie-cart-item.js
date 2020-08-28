4c4a47e502072e05f6d4e1a2011867d421d1afe8
Ridwan Yusuf <alabarise@gmail.com>
2020-08-29 00:54:29 +0100

    cookies item js

---
+++ ecommerce-cookie-cart-item.js
@@ -0,0 +1,10 @@
+17: {quantity: 1}
+18: {quantity: 1}
+20: {quantity: 5}
+21: {quantity: 1}
+23: {quantity: 2}
+26: {quantity: 1}
+28: {quantity: 1}
+31: {quantity: 1}
+33: {quantity: 1}
+57: {quantity: 1}
